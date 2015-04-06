805bcd2bc815dcda2bbd5fb0a1f774985fa44433
Kedar Kolluri <kkolluri@lbl.gov>
2015-04-06 19:41:46 -0400

    some edits to neb preparation methods

--- script_create_anneal_multiple_Illite.bash
+++ script_create_anneal_multiple_Illite.bash
@@ -107,7 +107,7 @@ awk '{if($1=="processing") {p = $4}; if($1=="atom") print p " " $3 " " $11 " " $
 ### PLEASE READ in FULL BEFORE RUNNING THIS-
 ### It has all steps some of which you may not want to use every time!
 
-for file in {1..20}
+for file in {4..6}
 do
   cd $file/;
   pwd
@@ -115,19 +115,37 @@ do
   ## the file dat.0.gz is the starting point (from annealing)
   ## if MD was not done or dat_lammps.0.gz exists, skip this step and go to second step where the size is replicated if needed
 
-  #lbnl_processor_exec.out start 0 end 2 interval 22 CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.coords-first SAVE_LAMMPS CHARGE MOLECULE > out.out 2>/dev/null
+  cp dat_lammps.postMD.gz dat_lammps.00.gz
+  gunzip dat_lammps.00.gz
+  lmp_openmpi_1214 -in /Users/KedarKolluri/Documents/projects/LBNL/expts/scripts/neb/make_input_files/in.illite.minimize -screen none -log log.minimize_replicated
+
+  mv dat.40000.gz dat.0.gz; rm dat_lammps.00;
+
+  lbnl_processor_exec.out start 0 end 2 interval 22 CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.coords-first SAVE_LAMMPS CHARGE MOLECULE > out.out 2>/dev/null
 
   ## change the file to something else so it is saved
 
-  #awk 'BEGIN{tag=0} {if($1=="NeighborAnalyses"){if(tag==0) {tag=1;} else {tag=0;} } if(tag==1) print $0}' out.out | awk '{if(NR>1) print $0}' > input.data
-  #mv dat.0.gz dat.postMD.gz
-  #cp dat_lammps.0.gz dat_lammps.postMD.gz
+  awk 'BEGIN{tag=0} {if($1=="NeighborAnalyses"){if(tag==0) {tag=1;} else {tag=0;} } if(tag==1) print $0}' out.out | awk '{if(NR>1) print $0}' > input.data
+  mv dat.0.gz dat.postMD.gz
+  cp dat_lammps.0.gz dat_lammps.postMD.gz
 
   ### step 2:
   ## run this step if we need to replicate the cell for whatever reasons (so that we can do NEB on bigger system where migration does not see it's image)
   ## this should use dat_lammps.postMD.gz as input
   ## please check before running
-  lmp_git_openmpi_021415 -in /Users/KedarKolluri/Documents/projects/LBNL/expts/scripts/neb/make_input_files/in.start_replicate -screen none -log log.replicate
+  lmp_git_openmpi_021415 -in /Users/KedarKolluri/Documents/projects/LBNL/expts/scripts/neb/make_input_files/in.start_replicate_nobonds -screen none -log log.replicate
+
+## Because replicate will need bonds to not exist, it is not minimized
+## So, we need to create those bonds using lbnl_processor and then
+## rerun lammps and minimize the structure
+
+  lbnl_processor_exec.out start 0 end 2 interval 22 CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.coords-first SAVE_LAMMPS CHARGE MOLECULE > out.create_bonds 2>&1
+
+  gunzip dat_lammps.0.gz; mv dat_lammps.0 dat_lammps.00
+
+  lmp_openmpi_1214 -in /Users/KedarKolluri/Documents/projects/LBNL/expts/scripts/neb/make_input_files/in.illite.minimize -screen none -log log.minimize_replicated
+
+  mv dat.40000.gz dat.0.gz; rm dat_lammps.00;
 
   ### step 3:
   # #the file is now saved as dat.0.gz and converted to dat_lammps.0.gz which is then converted to dat_lammps.begin
@@ -315,3 +333,13 @@ for file in {1..20}
 do
   cat $file/barriers.collate.final | sort -n >> energies.all
 done
+
+
+awk
+{
+  if($7=="good")
+    { HI=$5; LOW=$6; if(HI<LOW) {T=HI; HI=LOW;LOW=T;}; printf("%lf %lf %d %d \n", HI, LOW,$1,$2)
+    }
+}
+
+' quality.data.cleaned
