f4eff4fb038744c4aaa376a15a97ff8b4631079a
KKolluri <k.kedar@gmail.com>
2015-06-24 20:43:40 -0400

    created new folder for widths-related work

--- scripts_diff_widths/create_width_configs.bash
+++ scripts_diff_widths/create_width_configs.bash
@@ -1,5 +1,5 @@
 ### script for creating Cs structures
-####
+
 ## first take the structure and cut it in half
 ## next create illite out of it
 ## remember to set the sed srand(1) in c++ file before running this
@@ -10,12 +10,63 @@
 #wc 6by4by1_structure-Si2Al.xyz_data | awk '{print $1-1}' > 6by4by1_structure-Si2Al.xyz
 
 
+#### THIS awk is for when we move both layers
+awk 'BEGIN{
+      VAL='$iter'*0.2*'$vval';
+      XVAL=-0.09871188639278138*VAL;
+      ZVAL= 0.9951160552843967*VAL;}
+    {
+      if(NR==2)
+      {
+        printf("%lf %lf %lf %lf %lf %lf\n", $1, $2, $3+VAL, $4, $5, $6)
+      }else
+      {
+        if($4 > 9.8)
+        {
+          if(($1=="K")||($1=="Gh"))
+          {
+            printf("%s %lf %lf %lf\n", $1, $2+XVAL/4.0, $3, $4+ZVAL/4.0)
+          } else
+          {
+            printf("%s %lf %lf %lf\n", $1, $2+XVAL/2.0, $3, $4+ZVAL/2.0)
+          }
+        }else
+        {
+          if(($1=="K")||($1=="Gh"))
+          {
+            printf("%s %lf %lf %lf\n", $1, $2-XVAL/4.0, $3, $4-ZVAL/4.0)
+          } else
+          {
+            print $0
+          }
+        }
+      }
 
+    }
+    ' USETHIS-6by4by2_structure-Si2Al.xyz > 6by4by1_structure-Si2Al-$iter.xyz
+###
 
-lbnl_processor_latest_exec.out convert_VESTA filename 6by4by2_structure-Si2Al.xyz make_illite keep_ghosts CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.make rand_seed 1 SAVE_LAMMPS CHARGE MOLECULE > out.out
-cp dat_VESTA.20.xyz USETHIS-6by4by2_structure-Si2Al.xyz
+create_structfile()
+{
+  seedforfile=1
+  filename="USETHIS-6by4by2_structure-Si2Al.xyz"
+  if [ ! -z "$1" ]
+  then
+    seedforfile=$1
+  fi
+  if [ ! -z "$2" ]
+  then
+    filename=$2
+  fi
+  echo $seedforfile
+  lbnl_processor_latest_exec.out convert_VESTA filename 6by4by2_structure-Si2Al.xyz make_illite keep_ghosts CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.make rand_seed $seedforfile SAVE_LAMMPS CHARGE MOLECULE > out.out
+  cp dat_VESTA.20.xyz $filename
 
-****COPY SECOND LINE OF 6by4by2_structure-Si2Al.xyz to second line of USETHIS-6by4by2_structure-Si2Al.xyz*************************
+  awk '{if(NR==1) print $0}' $filename > modif-$filename
+  awk '{if(NR==2) print $0}' 6by4by2_structure-Si2Al.xyz >> modif-$filename
+  awk '{if(NR>2) print $0}' USETHIS-6by4by2_structure-Si2Al.xyz >> modif-$filename
+  mv modif-$filename $filename
+}
 ######
 
 if [ -d lammps_files ]
@@ -25,14 +76,16 @@ fi
 mkdir lammps_files
 
 vval=1
+initval=-2.0
 for itermain in {1..20}
 do
+  create_structfile $itermain
   mkdir lammps_files_$itermain
-  for iter in {0..40}
+  for iter in `seq 0 2 60`
   do
 
     awk 'BEGIN{
-          VAL='$iter'*0.2*'$vval';
+          VAL='$iter'*0.1*'$vval'+'$initval';
           XVAL=-0.09871188639278138*VAL;
           ZVAL= 0.9951160552843967*VAL;}
         {
@@ -45,16 +98,16 @@ do
             {
               if(($1=="K")||($1=="Gh"))
               {
-                printf("%s %lf %lf %lf\n", $1, $2+XVAL/4.0, $3, $4+ZVAL/4.0)
+                printf("%s1 %lf %lf %lf\n", $1, $2, $3, $4)
               } else
               {
-                printf("%s %lf %lf %lf\n", $1, $2+XVAL/2.0, $3, $4+ZVAL/2.0)
+                print $0
               }
             }else
             {
               if(($1=="K")||($1=="Gh"))
               {
-                printf("%s %lf %lf %lf\n", $1, $2-XVAL/4.0, $3, $4-ZVAL/4.0)
+                printf("%s %lf %lf %lf\n", $1, $2-XVAL/2.0, $3, $4-ZVAL/2.0)
               } else
               {
                 print $0
@@ -64,9 +117,9 @@ do
 
         }
         ' USETHIS-6by4by2_structure-Si2Al.xyz > 6by4by1_structure-Si2Al-$iter.xyz
-    lbnl_processor_latest_exec.out convert_VESTA filename 6by4by1_structure-Si2Al-$iter.xyz CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.make-cs rand_seed $itermain SAVE_LAMMPS CHARGE MOLECULE > out.$iter 2>&1
+    lbnl_processor_latest_exec.out convert_VESTA filename 6by4by1_structure-Si2Al-$iter.xyz CUTOFF_FILE /Users/KedarKolluri/lib/cutoff_file.illite.make-cs-k1gh1 rand_seed $itermain SAVE_LAMMPS CHARGE MOLECULE > out.$iter 2>&1
     mv dat_lammps.20 lammps_files/dat_lammps.$iter
-    rm dat_VESTA.20.xyz dat_VESTA.10.xyz dat_lammps.10
+    rm dat_VESTA.20.xyz
   done
   mv lammps_files/* lammps_files_$itermain/
 done
@@ -80,7 +133,7 @@ do
   rm -rf r$rootfile
   fi
   mkdir r$rootfile
-  sed -e 's/SEED/'$rootfile'/' in.addcesium_T > r$rootfile/in.addcesium_template;
+  sed -e 's/SEED/'$rootfile'/' /Users/KedarKolluri/Documents/projects/LBNL/expts/scripts/scripts_diff_widths/in.addcesium_T > r$rootfile/in.addcesium_template;
   cd r$rootfile
   for file in `seq 0 10 101`
   do
@@ -95,7 +148,7 @@ do
     fi
     for file2 in `seq 0 2 41`
     do
-      cp ../../prelim_single/lammps_files_1/dat_lammps.$file2 dat_lammps.00
+      cp ../../prelim_single_many_random/lammps_files_1/dat_lammps.$file2 dat_lammps.00
       if [ -e dat_lammps.00 ]
       then
         lmp_git_openmpi_021415 -in in.addcesium -screen none -log log.$file2
@@ -109,27 +162,38 @@ do
 done
 
 ## for 0 and 100 only
-
-for file in `seq 1 1 20`
+for folder in `seq 0 100 10`
 do
-  echo $file
-  mkdir r$file
-  cp in.addcesium r$file/
-  cd r$file
-  pwd
-  if [ -e en.output ]
+  foldername=cs$folder-reps
+  if [ -d $foldername ]
   then
-    rm en.output
+    rm -rf $foldername/
   fi
-  for file2 in `seq 0 2 40`
+  mkdir -p $foldername
+  cd $foldername
+
+  for file in `seq 1 1 20`
   do
-    cp ../../prelim_single_many_random/lammps_files_$file/dat_lammps.$file2 dat_lammps.00
-    if [ -e dat_lammps.00 ]
+    echo $file
+    mkdir r$file
+    cp /Users/KedarKolluri/Documents/projects/LBNL/expts/scripts/scripts_diff_widths/in.addcesium_K1G1_${folder}Cs r$file/in.addcesium_K1G1
+    cd r$file
+    pwd
+    if [ -e en.output ]
     then
-      lmp_git_openmpi_021415 -in in.addcesium -screen none -log log.$file2
-      awk '{if($1==40000) printf("%d %lf %lf %lf %lf\n", '$file2', $3, $4, $5, $6)}' log.$file2 >> en.output
-      mv dat.40000.gz dat.$file2.gz;
+      rm en.output
     fi
+    for file2 in `seq 0 2 40`
+    do
+      cp ../../prelim_single_many_random/lammps_files_$file/dat_lammps.$file2 dat_lammps.00
+      if [ -e dat_lammps.00 ]
+      then
+        lmp_git_openmpi_021415 -in in.addcesium_K1G1 -screen none -log log.$file2
+        awk '{if($1==40000) printf("%d %lf %lf %lf %lf\n", '$file2', $3, $4, $5, $6)}' log.$file2 >> en.output
+        mv dat.40000.gz dat.$file2.gz;
+      fi
+    done
+    cd ../
   done
   cd ../
 done
